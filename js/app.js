8ad7f079ec8fa7bd85e97c5360fe4780e6cadc4d
CarlosCarrilloK <165876023+CarlosCarrilloK@users.noreply.github.com>
2024-04-02 23:36:45 -0700

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+ 
+    // Función para obtener el año actual
+    const getCurrentYear = () => {
+      return new Date().getFullYear();
+    }
+  
+    // Actualiza el año actual
+    document.querySelector("#current-year").textContent = getCurrentYear();
+  
+  
+  });
